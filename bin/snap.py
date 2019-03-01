5d6e18d9ee8b519414013c98c0c41c5d3e527755
mars0007 <marcin.jarosinski@gmail.com>
2019-02-28 22:19:04 -0500

    handle error when stopping a pending instance or starting a stopping one

--- bin/snap.py
+++ bin/snap.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+import botocore
 
 session = boto3.Session(profile_name='default') 
 ec2 = session.resource('ec2') 
@@ -133,7 +134,11 @@ def stop_instances(project):
     
     for i in instances:
         print('Stopping {0}...'.format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -147,7 +152,11 @@ def stop_instances(project):
     
     for i in instances:
         print('Starting {0}...'.format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
